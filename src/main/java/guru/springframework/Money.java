0a8d33255110afca61b08e67fd694de5cec167da
gzacharski <g.zacharski@wp.pl>
2020-09-23 21:30:34 +0200

    Refactoring classes

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -3,16 +3,27 @@ package guru.springframework;
 import java.util.Objects;
 
 public abstract class Money {
+
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public abstract Money times(int multiplier);
 
     public static Money dollar(int amount){
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount){
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    protected String currency() {
+        return currency;
     }
 
     @Override
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -16,9 +16,8 @@ public class MoneyTest {
 
     @Test
     void testEqualityDollar() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(8));
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(8));
     }
 
     @Test
@@ -30,7 +29,14 @@ public class MoneyTest {
 
     @Test
     void testEqualityFranc() {
-        assertEquals(Money.franc(5), new Franc(5));
-        assertNotEquals(Money.franc(5), new Franc(8));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.dollar(8));
+    }
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD",Money.dollar(1).currency());
+        assertEquals("CHF",Money.franc(1).currency());
+
     }
 }
