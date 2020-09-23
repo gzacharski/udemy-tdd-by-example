150585f7da28d7be4041d70f1ccfddce6aea4950
gzacharski <g.zacharski@wp.pl>
2020-09-23 17:41:18 +0200

    equality test 2

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
@@ -18,5 +19,6 @@ public class MoneyTest {
     @Test
     void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
