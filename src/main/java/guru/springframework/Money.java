a500c70edbc5d78631b39724f69136790c5af0ee
gzacharski <g.zacharski@wp.pl>
2020-09-23 18:50:49 +0200

    Update classes

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,4 +1,20 @@
 package guru.springframework;
 
-public class Money {
+import java.util.Objects;
+
+public abstract class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
