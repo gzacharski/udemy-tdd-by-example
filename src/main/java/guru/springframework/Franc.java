09f8300ef92d536c9cac3a8c16e8447a99a906b4
gzacharski <g.zacharski@wp.pl>
2020-09-23 19:21:11 +0200

    Update classes

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -6,7 +6,8 @@ public class Franc extends Money{
         this.amount=amount;
     }
 
-    Franc times(int multiplier){
+    @Override
+    public Money times(int multiplier){
         return new Franc(amount*multiplier);
     }
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -5,6 +5,16 @@ import java.util.Objects;
 public abstract class Money {
     protected int amount;
 
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
